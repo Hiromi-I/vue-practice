c3e681b1431847a25f1a4e657d5a7bdcceaed93e
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2021-01-20 23:14:58 +0900

    モーダルコンポーネントを移動

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
     <router-view />
-    <TabHolder />
-    <ModalSet />
     <router-link to="/">トップ</router-link>
     <router-link to="/modal-sample">モーダル</router-link>
     <router-link to="/loading-sample">ローディング</router-link>
@@ -10,14 +8,7 @@
 </template>
 
 <script>
-import TabHolder from "./components/TabHolder.vue";
-import ModalSet from "./components/ModalSet.vue";
-
 export default {
   name: "App",
-  components: {
-    TabHolder,
-    ModalSet
-  }
 };
 </script>
