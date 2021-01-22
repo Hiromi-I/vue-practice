ef2f3ec7d3cb29b8d7270020c20d5c7208b06767
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2021-01-22 21:58:13 +0900

    BindingTextAnimationを仮表示

--- src/views/TextAnimation.vue
+++ src/views/TextAnimation.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <SCSSTextAnimation />
+    <BindingTextAnimation />
   </div>
 </template>
 
 <script>
 import SCSSTextAnimation from "@/components/SCSSTextAnimation";
+import BindingTextAnimation from "@/components/BindingTextAnimation";
 
 export default {
   components: {
     SCSSTextAnimation,
+    BindingTextAnimation,
   }
 };
 </script>
